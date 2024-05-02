55c1d807e2b4d0322bbbb92eae2de3a6e0bb3b9a
joao magnago <joaomagnago.wm@gmail.com>
2024-05-02 19:39:20 -0300

    feat: create avatar component and stories

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { Avatar, AvatarProps } from '@ignite-ui-magnago/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data dispaly/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/joaomagnago.png',
+    alt: 'Foto de perfil do João Magnago'
+  }
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {
+}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined
+  }
+}
